01f252c1e2bed7461a74b6723984afe10515d735
joaolinux <joaomotionless570@gmail.com>
2022-08-09 13:12:15 -0300

    Add files via upload

---
+++ Entrada De Dados/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //scanf("tipo_de_entrada", &var1, &var2);
+    //scanf(%tipo1%tipo2", &var1, &var2);
+
+    int num1, num2;
+    printf("Por favor, digite um numero: \n");
+    scanf("%i", &num1);
+
+    printf("Voce digitou: %i", num1);
+
+return 0;
+}
